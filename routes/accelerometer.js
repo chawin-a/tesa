18cba63f838914dd9a0b46343f3459467cd7e61b
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2018-01-11 11:26:28 +0700

    test

--- routes/accelerometer.js
+++ routes/accelerometer.js
@@ -1,8 +1,10 @@
 var express = require('express');
-var request = require('request');
+var request = require('../function/request-async');
 var Accelerometer = require('../models/Accelerometer');
 var router = express.Router();
 var accelerometer = require("../controllers/AccelerometerController.js");
+
+var urls = require('../config/urls');
 
 // Get all accelerometer
 router.get('/', function(req, res) {
@@ -10,49 +12,59 @@ router.get('/', function(req, res) {
 });
 
 router.get('/get', function(req, res) {
-    request('http://10.0.0.10/api/accelerometer/29', function(error, res, body) {
-        var data = JSON.parse(body);
-        // console.log(body);
-        // console.log(data);
-        for(var i=0;i<data["data"].length;i++) {
-            var q = new Accelerometer(data["data"][i]);
-            q.save(function(err) {
-                if(err) {
-                    console.log(err);
-                }
-            })
-        }
-    })
+  var requestData = [];
+  for(var i=1;i<=urls.teams;i++) {
+    requestData.push(request(urls.url + 'accelerometer/' + i));
+  }  
+  Promise.all(requestData)
+  .then(function(data) {
+    for(var i=0;i<data.length;i++) {
+      console.log(data[i]);
+    }
+  })
+    // request('http://10.0.0.10/api/accelerometer/29', function(error, res, body) {
+    //     var data = JSON.parse(body);
+    //     // console.log(body);
+    //     // console.log(data);
+    //     for(var i=0;i<data["data"].length;i++) {
+    //         var q = new Accelerometer(data["data"][i]);
+    //         q.save(function(err) {
+    //             if(err) {
+    //                 console.log(err);
+    //             }
+    //         })
+    //     }
+    // })
 });
 
-// Get single accelerometer by id
-router.get('/show/:id', function(req, res) {
-  accelerometer.show(req, res);
-});
+// // Get single accelerometer by id
+// router.get('/show/:id', function(req, res) {
+//   accelerometer.show(req, res);
+// });
 
-// Create accelerometer
-router.get('/create', function(req, res) {
-  accelerometer.create(req, res);
-});
+// // Create accelerometer
+// router.get('/create', function(req, res) {
+//   accelerometer.create(req, res);
+// });
 
-// Save accelerometer
-router.post('/save', function(req, res) {
-  accelerometer.save(req, res);
-});
+// // Save accelerometer
+// router.post('/save', function(req, res) {
+//   accelerometer.save(req, res);
+// });
 
-// Edit accelerometer
-router.get('/edit/:id', function(req, res) {
-  accelerometer.edit(req, res);
-});
+// // Edit accelerometer
+// router.get('/edit/:id', function(req, res) {
+//   accelerometer.edit(req, res);
+// });
 
-// Edit update
-router.post('/update/:id', function(req, res) {
-  accelerometer.update(req, res);
-});
+// // Edit update
+// router.post('/update/:id', function(req, res) {
+//   accelerometer.update(req, res);
+// });
 
-// Edit update
-router.post('/delete/:id', function(req, res, next) {
-  accelerometer.delete(req, res);
-});
+// // Edit update
+// router.post('/delete/:id', function(req, res, next) {
+//   accelerometer.delete(req, res);
+// });
 
 module.exports = router;
